a9933e4aef3a19c1acc1142bec7dde12ebf5a3c4
ygorpn <ygor-nogueira@hotmail.com>
2022-08-16 22:46:42 -0300

    estilizando menu mobile e deixando com acessibilidade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-const btnMobile = document.getElementById('hamburguer');
+const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+
+  if(event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
-  nav.classlist.toggle('active');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if(activ) { 
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+  }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
